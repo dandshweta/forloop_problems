f99a98943acf38078864387420b7559b700781d0
shweta dand <dandshweta2017@gmail.com>
2023-04-06 17:06:35 +0530

    first commit

---
+++ day06-04/1st.js
@@ -0,0 +1,8 @@
+for (let i = 0; i <= 1000; i++) {
+  console.log(i);
+  i = i + 99;
+}
+
+// for (let i = 0; i <= 1000; i += 100) {
+//   console.log(i);
+// }
---
+++ day06-04/4th.js
@@ -0,0 +1,3 @@
+for (let i = 3; i <= 15; i += 3) {
+  console.log(i);
+}
